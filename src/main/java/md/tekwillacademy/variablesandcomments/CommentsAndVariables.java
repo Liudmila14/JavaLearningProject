e80c9156b1237217835177453c04e8241108bc11
lfedulova <fedulovaliudmila1@gmail.com>
2024-04-18 17:39:28 +0300

    I have added 12 new primitive variables as a practical exercise

--- src/main/java/md/tekwillacademy/variablesandcomments/CommentsAndVariables.java
+++ src/main/java/md/tekwillacademy/variablesandcomments/CommentsAndVariables.java
@@ -2,10 +2,45 @@ package md.tekwillacademy.variablesandcomments;
 
 public class CommentsAndVariables {
     public static void main(String[] args) {
-        boolean isHotOutside;
-        isHotOutside=true;
-        System.out.println("The value of our variable is: "+" isHotOutside");
-        boolean isFriday=true;
-        System.out.println("The value of our variable is: "+" isFriday");
+        boolean isTekwillCourseInteresting;
+       isTekwillCourseInteresting = true;
+        System.out.println("The value of variable is: "+ isTekwillCourseInteresting);
+       boolean isTekwillCourseBoring=false;
+        System.out.println("The value of our variable is: " + isTekwillCourseBoring);
+
+        boolean falseBooleanVariable=false;
+        boolean trueBooleanVariable=true;
+        char firstCharFromAscii='A';
+        char lastCharFromAscii='Z';
+        byte lowerLimitOfByteDataType=-128;
+        byte upperLimitOfByteDataType=127;
+        short lowerLimitOfShortDataType=-32768;
+        short upperLimitOfShortDataType=32767;
+        int lowerLimitOfIntDataType=-2147483648;
+        int upperLimitOfIntDataType=2147483647;
+        long lowerLimitOfLongDataType=-9223372036854775808L;
+        long upperLimitOfLongDataType=922337203685477807L;
+        float lowerLimitOfFloatDataType=-3.4E+38F;
+        float upperLimitOfFloatDataType=3.4E+38F;
+        double lowerLimitOfDoubleDataType=-1.7E+308;
+        double upperLimitOfDoubleDataType=1.7E+308;
+        System.out.println(falseBooleanVariable);
+        System.out.println(trueBooleanVariable);
+        System.out.println(firstCharFromAscii);
+        System.out.println(lastCharFromAscii);
+        System.out.println(lowerLimitOfByteDataType);
+        System.out.println(upperLimitOfByteDataType);
+        System.out.println(lowerLimitOfShortDataType);
+        System.out.println(upperLimitOfShortDataType);
+        System.out.println(lowerLimitOfIntDataType);
+        System.out.println(upperLimitOfIntDataType);
+        System.out.println(lowerLimitOfLongDataType);
+        System.out.println(upperLimitOfLongDataType);
+        System.out.println(lowerLimitOfFloatDataType);
+        System.out.println(upperLimitOfFloatDataType);
+        System.out.println(lowerLimitOfDoubleDataType);
+        System.out.println(upperLimitOfDoubleDataType);
+
+
     }
 }
